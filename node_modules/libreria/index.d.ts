a952b216f7ca2f0c3a2ef937d2803651aafdacfc
305 <305@academicos.cesde.edu.co>
2018-11-27 17:35:03 -0500

    Commint inicial

---
+++ node_modules/libreria/index.d.ts
@@ -0,0 +1,8 @@
+import { ModuleWithProviders } from '@angular/core';
+export * from './sample.component';
+export * from './sample.directive';
+export * from './sample.pipe';
+export * from './sample.service';
+export declare class SampleModule {
+    static forRoot(): ModuleWithProviders;
+}
---
+++ node_modules/libreria/index.js
@@ -0,0 +1,118 @@
+import { Component, Directive, ElementRef, Injectable, NgModule, Pipe } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+var SampleComponent = (function () {
+    function SampleComponent() {
+    }
+    return SampleComponent;
+}());
+SampleComponent.decorators = [
+    { type: Component, args: [{
+                selector: 'sample-component',
+                template: "<h1>Sample component</h1>"
+            },] },
+];
+/**
+ * @nocollapse
+ */
+SampleComponent.ctorParameters = function () { return []; };
+
+var SampleDirective = (function () {
+    /**
+     * @param {?} el
+     */
+    function SampleDirective(el) {
+        this.el = el;
+    }
+    return SampleDirective;
+}());
+SampleDirective.decorators = [
+    { type: Directive, args: [{
+                selector: '[sampleDirective]'
+            },] },
+];
+/**
+ * @nocollapse
+ */
+SampleDirective.ctorParameters = function () { return [
+    { type: ElementRef, },
+]; };
+
+/**
+ * Transforms any input value
+ */
+var SamplePipe = (function () {
+    function SamplePipe() {
+    }
+    /**
+     * @param {?} value
+     * @param {?=} args
+     * @return {?}
+     */
+    SamplePipe.prototype.transform = function (value, args) {
+        if (args === void 0) { args = null; }
+        return value;
+    };
+    return SamplePipe;
+}());
+SamplePipe.decorators = [
+    { type: Pipe, args: [{
+                name: 'samplePipe'
+            },] },
+    { type: Injectable },
+];
+/**
+ * @nocollapse
+ */
+SamplePipe.ctorParameters = function () { return []; };
+
+var SampleService = (function () {
+    function SampleService() {
+    }
+    return SampleService;
+}());
+SampleService.decorators = [
+    { type: Injectable },
+];
+/**
+ * @nocollapse
+ */
+SampleService.ctorParameters = function () { return []; };
+
+var SampleModule = (function () {
+    function SampleModule() {
+    }
+    /**
+     * @return {?}
+     */
+    SampleModule.forRoot = function () {
+        return {
+            ngModule: SampleModule,
+            providers: [SampleService]
+        };
+    };
+    return SampleModule;
+}());
+SampleModule.decorators = [
+    { type: NgModule, args: [{
+                imports: [
+                    CommonModule
+                ],
+                declarations: [
+                    SampleComponent,
+                    SampleDirective,
+                    SamplePipe
+                ],
+                exports: [
+                    SampleComponent,
+                    SampleDirective,
+                    SamplePipe
+                ]
+            },] },
+];
+/**
+ * @nocollapse
+ */
+SampleModule.ctorParameters = function () { return []; };
+
+export { SampleModule, SampleComponent, SampleDirective, SamplePipe, SampleService };
